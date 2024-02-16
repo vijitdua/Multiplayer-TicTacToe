2db9531a4fef8ecc7c39f614c000814cb1a410db
Vijit Dua <82555472+vijitdua@users.noreply.github.com>
2024-02-15 20:58:13 -0800

    random commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,10 @@
 import './App.css';
-import SignUp from './components/SignUp.js'
-import Login from "./components/Login.js"
-import {StreamChat} from "stream-chat";
-import Cookies from "universal-cookie";
+import SignUp from "./components/SignUp";
 
 function App() {
-
-    const streamAPIKey = process.env.STREAM_API_KEY;
-    const cookies = new Cookies();
-    const token = cookies.get("token"); // Check if user is already signed in
-    const streamServerClient = StreamChat.getInstance(streamAPIKey);
-
-    if (token) {
-        streamServerClient.connectUser({
-            id: cookies.get("userID"),
-            name: cookies.get("username"),
-            firstName: cookies.get("firstName"),
-            lastName: cookies.get("lastName"),
-            hashedPassword: cookies.get("hashedpassword")
-        },
-            token).then( (user) => {
-                console.log(user);
-        } );
-    }
-
-    return (
-        <div className="App">
-            <SignUp/>
-            <Login/>
-        </div>
-    );
+  return (
+    <SignUp />
+  );
 }
 
 export default App;
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,33 +1,42 @@
-import React, {useState} from "react";
-import Axios from "axios";
-import Cookies from "universal-cookie";
+import {useState} from "react";
+import "./SignUp.css";
+
+
+// import axios from "axios";
 
 function SignUp() {
     const [user, setUser] = useState(null);
-    const cookies = new Cookies();
 
-    const signUp = () => {
-        Axios.post("http://localhost:3005/signup", user).then(res => { // Send data to server //TODO: change to env port
-            const {token, userID, firstName, lastName, username, hashedPassword} = res.data; // Destructure data returned from server into frontend
-
-            // Store cookies of user login data
-            cookies.set("token", token);
-            cookies.set("userID", userID);
-            cookies.set("firstName", firstName);
-            cookies.set("lastName", lastName);
-            cookies.set("username", username);
-            cookies.set("hashedPassword", hashedPassword);
-        });
+    /**
+     * setUserData: Changes data of User Object
+     * @param dataType Key of the data of modify
+     * @param data Value to link to key
+     */
+    function setUserData(dataType, data) {
+        setUser({...user, [dataType]: data});
     }
 
-    return (<div className="signUp">
-            <label> Sign Up </label>
-            <input placeholder="First Name" onChange={(event) => setUser({...user, firstName: event.target.value})}/>
-            <input placeholder="Last Name" onChange={(event) => setUser({...user, lastName: event.target.value})}/>
-            <input placeholder="Username" onChange={(event) => setUser({...user, username: event.target.value})}/>
-            <input type="password" placeholder="Password"
-                   onChange={(event) => setUser({...user, password: event.target.value})}/>
-            <button onClick={signUp}> Sign Up</button>
+    // async function getUserData(){
+    //     const response = await axios.get(`http://${process.env["SERVER_PORT "]}`)
+    // }
+
+    function sendUserData() {
+        console.log(`http://localhost:3005`);
+    }
+
+    return (
+        <div className="SignUp">
+            <form>
+                <input type="text" placeholder="First Name"
+                       onChange={(event) => setUserData("firstName", event.target.value)}/>
+                <input type="text" placeholder="Last Name"
+                       onChange={(event) => setUserData("lastName", event.target.value)}/>
+                <input type="text" placeholder="username"
+                       onChange={(event) => setUserData("username", event.target.value)}/>
+                <input type="password" placeholder="password"
+                       onChange={(event) => setUserData("password", event.target.value)}/>
+                <button type="button" onClick={sendUserData}>Sign Up!</button>
+            </form>
         </div>
     );
 }
